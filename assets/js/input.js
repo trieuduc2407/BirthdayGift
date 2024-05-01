23f52b78964f69798c109c811df01073773e9edc
trieuduc2407 <trieuduc2407@gmail.com>
2024-05-01 23:35:19 +0700

    test 2

--- assets/js/input.js
+++ assets/js/input.js
@@ -4,7 +4,7 @@ const modal = new bootstrap.Modal(document.getElementById("exampleModal"));
 btn.addEventListener("click", () => {
     let name = document.getElementById("name").value;
     let date = document.getElementById("date").value;
-    if (name == 'Vũ Quế Anh' && date == '01/05/2024') {
+    if (name == 'Nghiêm Minh Châu' && date == '16/09/2004') {
         window.location.href = 'card.html';
     } else {
         modal.show()
